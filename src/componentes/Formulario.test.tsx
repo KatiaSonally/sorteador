e782f8a3552b1e7b8e2e3d9d84dcd90617e278c2
KatiaSonally <katiasonally@yahoo.com.br>
2023-09-09 14:43:25 -0300

    feat: implements the test for including participants

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -1,23 +1,40 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
 
-// Jest
-
 test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
-
     render(<Formulario />)
-
     // encontrar no DOM o input
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    // encontrar o botão
+    const botao = screen.getByRole('button')
+    // garantir que o input esteja no documento
+    expect(input).toBeInTheDocument()
+    // garantir que o botão esteja desabilitado
+    expect(botao).toBeDisabled()
+})
 
+test('adicionar um participante caso exista um nome preenchido', () => {
+    render(<Formulario />)
+    // encontrar no DOM o input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
     // encontrar o botão
     const botao = screen.getByRole('button')
 
-    // garantir que o input esteja no documento
-    expect(input).toBeInTheDocument()
+    // inserir um valor no input
+    fireEvent.change(input, {
+        target: {
+            value: 'Ana Catarina'
+        }
+    })
 
-    // garantir que o botão esteja desabilitado
-    expect(botao).toBeDisabled()
+    // clicar no botão de submeter
+    fireEvent.click(botao)
+
+    // garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus()
+
+    // garantir que o input não tenha um valor
+    expect(input).toHaveValue("")
 
 })
